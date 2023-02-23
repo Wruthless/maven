fcc19ccd55211a4bda46a1f32ded1a17a998532c
W. Brandon Gonzales <107366291+Wruthless@users.noreply.github.com>
2023-02-23 14:36:42 -0500

    hello2 dependecy example.
    Added hello2, which simply demonstrates maven dependency inclusion.

---
+++ hello2/src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloWorld {
+
+	public static void main(String args[]) {
+		System.out.println();
+		System.out.println("Hello World");
+		System.out.printf("%s", StringUtils.capitalize("hello world"));
+	}
+
+} // end HelloWorld class
